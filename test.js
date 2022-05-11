2946f67ea71f3c2001aa65a239d1f7d07099fff9
Leon Ochmann <leonochmann@outlook.com>
2022-05-11 18:25:58 +0200

    (Almost) 100% code coverage for database.js

--- test.js
+++ test.js
@@ -11,7 +11,9 @@ const CHAT2 = 200
 const NO_CHAT = 999
 const USER1_1 = "user 1"
 const USER1_2 = "user 2"
+const USER1_3 = "user 3"
 const USER2_1 = "user 1"
+const NEW_USER = "user 10"
 const NO_USER = "user 99"
 
 test.before('start mongodb server', async t => {
@@ -29,10 +31,11 @@ test.beforeEach('populate with users', async t => {
 
 	await db.addUser(CHAT1, USER1_1)
 	await db.addUser(CHAT1, USER1_2)
+	await db.addUser(CHAT1, USER1_3)
 	await db.addUser(CHAT2, USER2_1)
 })
 
-test.afterEach('drop collection', async t => {
+test.afterEach.always('drop collection', async t => {
 	const db = t.context.db
 	await db.clear()
 })
@@ -46,10 +49,11 @@ test.serial('assign average score on user creation', async t => {
 	const db = t.context.db
 
 	const result = await db.updateScore(CHAT1, USER1_1, 5)
-	await db.addUser(CHAT1, "user 3")
+	await db.addUser(CHAT1, NEW_USER)
 	const users = await db.getUsers(CHAT1)
 	const user1_1 = _.find(users, user => user.userID === USER1_1)
-	const user3 = _.find(users, user => user.userID === "user 3")
+	const user1_2 = _.find(users, user => user.userID === USER1_2)
+	const new_user = _.find(users, user => user.userID === NEW_USER)
 
 	t.like(user1_1, {
 		chatID: CHAT1,
@@ -57,22 +61,29 @@ test.serial('assign average score on user creation', async t => {
 		vacation: false,
 		score: 5,
 	})
-	t.like(user3, {
+	t.like(user1_2, {
 		chatID: CHAT1,
-		userID: "user 3",
+		userID: USER1_2,
 		vacation: false,
-		score: (5 + 0) / 2,
+		score: 0,
+	})
+	t.like(new_user, {
+		chatID: CHAT1,
+		userID: NEW_USER,
+		vacation: false,
+		score: (5 + 0 + 0) / 3,
 	})
 })
 
 test.serial('do nothing when adding already existing user', async t => {
 	const db = t.context.db
 
+	const usersBefore = await db.getUsers(CHAT1)
 	const result = await db.addUser(CHAT1, USER1_1)
 	const users = await db.getUsers(CHAT1)
 
 	t.is(result, null)
-	t.is(users.length, 2)
+	t.is(users.length, usersBefore.length)
 })
 
 test.serial('no users for nonexistent chatID', async t => {
@@ -86,6 +97,7 @@ test.serial('no users for nonexistent chatID', async t => {
 test.serial('remove existing user', async t => {
 	const db = t.context.db
 
+	const usersBefore = await db.getUsers(CHAT1)
 	const result = await db.removeUser(CHAT1, USER1_1)
 	const users = await db.getUsers(CHAT1)
 
@@ -95,15 +107,111 @@ test.serial('remove existing user', async t => {
 		vacation: false,
 		score: 0,
 	})
-	t.is(users.length, 1)
+	t.is(users.length + 1, usersBefore.length)
 })
 
 test.serial('remove nonexistent user', async t => {
 	const db = t.context.db
 
+	const usersBefore = await db.getUsers(CHAT1)
 	const result = await db.removeUser(CHAT1, NO_USER)
 	const users = await db.getUsers(CHAT1)
 
 	t.is(result, null)
-	t.is(users.length, 2)
+	t.is(users.length, usersBefore.length)
+})
+
+test.serial('update score of vacationing user', async t => {
+	const db = t.context.db
+
+	await db.toggleVacation(CHAT1, USER1_1)
+	await db.toggleVacation(CHAT1, USER1_2)
+	await db.updateScore(CHAT1, USER1_1, 5)
+
+	const users = await db.getUsers(CHAT1)
+	const user1_1 = _.find(users, user => user.userID === USER1_1)
+	const user1_2 = _.find(users, user => user.userID === USER1_2)
+	t.like(user1_1, {
+		vacation: true,
+		score: 5,
+	})
+	t.like(user1_2, {
+		vacation: true,
+		score: 5 / 2,
+	})
+})
+
+test.serial('correct score compensation for vacationing users', async t => {
+	const db = t.context.db
+
+	const user1 = await db.toggleVacation(CHAT1, USER1_1)
+	await db.updateScore(CHAT1, USER1_2, 5)
+	await db.updateScore(CHAT1, USER1_3, 10)
+
+	const users = await db.getUsers(CHAT1)
+	const user1_1 = _.find(users, user => user.userID === USER1_1)
+	t.like(user1_1, {
+		vacation: true,
+		score: (10 + 5) / 2,
+	})
+})
+
+test.serial('call next with some users on vacation', async t => {
+	const db = t.context.db
+
+	await db.updateScore(CHAT1, USER1_1, 3)
+	await db.updateScore(CHAT1, USER1_2, 2)
+	await db.toggleVacation(CHAT1, USER1_1) // USER1_1 will get half of the points awarded in the following two commands
+	await db.updateScore(CHAT1, null, 4) // USER1_3 gets 4 points
+	await db.updateScore(CHAT1, null, 2) // USER1_2 gets +2 points, so 4
+	await db.toggleVacation(CHAT1, USER1_2) // now USER1_1 and USER1_2 will get the same points as USER1_3 in the next command
+	await db.updateScore(CHAT1, null, 3) // USER1_3 gets +3
+
+	const users = await db.getUsers(CHAT1)
+	const user1_1 = _.find(users, user => user.userID === USER1_1)
+	const user1_2 = _.find(users, user => user.userID === USER1_2)
+	const user1_3 = _.find(users, user => user.userID === USER1_3)
+	t.like(user1_1, {
+		vacation: true,
+		score: 3 + (4 + 2) / 2 + 3 / 1,
+	})
+	t.like(user1_2, {
+		vacation: true,
+		score: 2 + 2 + 3 / 1,
+	})
+	t.like(user1_3, {
+		vacation: false,
+		score: 0 + 4 + 3,
+	})
+})
+
+test.serial('call next with all users on vacation', async t => {
+	const db = t.context.db
+
+	await db.toggleVacation(CHAT1, USER1_1)
+	await db.toggleVacation(CHAT1, USER1_2)
+	await db.toggleVacation(CHAT1, USER1_3)
+
+	const result = await db.updateScore(CHAT1, null, 5)
+	t.like(result, {
+		err: 'zero users not on vacation',
+	})
+})
+
+test.serial('update score of nonexistent user', async t => {
+	const db = t.context.db
+
+	const result = await db.updateScore(CHAT1, NO_USER, 1)
+	t.like(result, {
+		err: 'user unknown'
+	})
+})
+
+test.serial('call next with zero users', async t => {
+	const db = t.context.db
+
+	const result = await db.updateScore(NO_CHAT, null, 1)
+	t.like(result, {
+		err: 'zero users'
+	})
 })
